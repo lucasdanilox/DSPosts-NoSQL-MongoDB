befff7d17b21b810c477144f36817ddfe52fda30
lucasdanilox <lukastecno@gmail.com>
2024-02-11 18:38:56 -0300

    GET user by id

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,6 +20,12 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> list = repository.findAll();
         return list.stream().map(UserDTO::new).collect(Collectors.toList());
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> result = repository.findById(id);
+        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+        return new UserDTO(entity);
     }
 
 }
