89617fd90f87e80aca215007ae9a70589602e212
lucasdanilox <lukastecno@gmail.com>
2024-02-11 14:45:14 -0300

    Project created

---
+++ src/main/java/com/devsuperior/workshopmongo/DsPostsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPostsApplication.class, args);
+	}
+
+}
