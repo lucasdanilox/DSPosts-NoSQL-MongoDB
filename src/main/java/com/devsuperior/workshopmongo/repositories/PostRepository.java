88c44d12539e2bb146fbbd21669dcacce455e772
lucasdanilox <lukastecno@gmail.com>
2024-02-11 15:25:16 -0300

    Fix: Author, Comment, Post

--- src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/devsuperior/workshopmongo/repositories/PostRepository.java
@@ -1,9 +1,9 @@
 package com.devsuperior.workshopmongo.repositories;
 
-import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.models.entities.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 }
